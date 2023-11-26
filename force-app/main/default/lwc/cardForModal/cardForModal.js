8e32745190f8dfea50bd62057d7cb2243ba64a3e
Giovannidjuric <giovanni_djuric@hotmail.nl>
2023-11-26 21:13:19 +0100

    Adding fields to form

--- force-app/main/default/lwc/cardForModal/cardForModal.js
+++ force-app/main/default/lwc/cardForModal/cardForModal.js
@@ -4,6 +4,8 @@ import getUserFullname from '@salesforce/apex/UserClass.getUserFullname'
 import { getPicklistValues } from "lightning/uiObjectInfoApi";
 import CASE_STATUS from "@salesforce/schema/Case.Status";
 import CASE_REASON from "@salesforce/schema/Case.Reason";
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import CASE_OBJECT from '@salesforce/schema/Case';
 
 export default class CardForModal extends LightningElement {
     @track showModal = false; 
@@ -12,20 +14,55 @@ export default class CardForModal extends LightningElement {
     masterRecordTypeId = "012000000000000AAA";
     caseStatusValues;
     caseReasonValues;
-    error;
+    recordTypes;
 
-    @wire(getPicklistValues, { recordTypeId: this.masterRecordTypeId, fieldApiName: CASE_STATUS})
+    @wire(getObjectInfo, { objectApiName: CASE_OBJECT })
+    objectInfo({error, data}) {
+        if(data) {
+             this.recordTypes = data.recordTypeInfos;
+            console.log(JSON.stringify(this.recordTypes));
+        } else {
+            console.log('error getting case info');
+            console.log(JSON.stringify(error));
+        }
+    }
+
+    @wire(getPicklistValues, { recordTypeId: "012000000000000AAA", fieldApiName: CASE_STATUS})
     getStatusValues({error, data}){
         if(data){
             console.log('yes, we found case status values!')
-            console.log(data);
-            this.caseStatusValues = data;
-            this.error = undefined;
+            console.log(JSON.stringify(data));
+            console.dir(data);
+            this.caseStatusValues = data.values.map((item) => {
+                return {'label': item.label, 'value': item.value}
+            })
+            console.log('caseStatusValues ' + JSON.stringify(this.caseStatusValues));
+
+
         } else if(error){
             console.log('oohnee, error retrieving case status values..');
-            console.log(error);
-            this.error = error;
+            console.log(JSON.stringify(error));
             this.caseStatusValues = undefined;
+
+        }
+    }
+
+    @wire(getPicklistValues, { recordTypeId: "012000000000000AAA", fieldApiName: CASE_REASON})
+    getReasonValues({error, data}){
+        if(data){
+            console.log('yes, we found reason status values!')
+            console.log(JSON.stringify(data));
+            console.dir(data);
+            this.caseReasonValues = data.values.map((item) => {
+                return {'label': item.label, 'value': item.value}
+            })
+            console.log('caseReasonValues ' + JSON.stringify(this.caseReasonValues));
+
+
+        } else if(error){
+            console.log('oohnee, error retrieving case reason values..');
+            console.log(error);
+            this.caseReasonValues = undefined;
 
         }
     }
