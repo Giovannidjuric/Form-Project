1db1e50f215ee26aee85a1b467b292376a3a9790
Giovannidjuric <giovanni_djuric@hotmail.nl>
2023-11-26 20:20:16 +0100

    Working on Form Features

--- force-app/main/default/lwc/cardForModal/cardForModal.js
+++ force-app/main/default/lwc/cardForModal/cardForModal.js
@@ -1,11 +1,34 @@
 import { LightningElement, track, wire} from 'lwc';
 import Id from "@salesforce/user/Id";
 import getUserFullname from '@salesforce/apex/UserClass.getUserFullname'
+import { getPicklistValues } from "lightning/uiObjectInfoApi";
+import CASE_STATUS from "@salesforce/schema/Case.Status";
+import CASE_REASON from "@salesforce/schema/Case.Reason";
 
 export default class CardForModal extends LightningElement {
     @track showModal = false; 
     userId = Id;
     fullName;
+    masterRecordTypeId = "012000000000000AAA";
+    caseStatusValues;
+    caseReasonValues;
+    error;
+
+    @wire(getPicklistValues, { recordTypeId: this.masterRecordTypeId, fieldApiName: CASE_STATUS})
+    getStatusValues({error, data}){
+        if(data){
+            console.log('yes, we found case status values!')
+            console.log(data);
+            this.caseStatusValues = data;
+            this.error = undefined;
+        } else if(error){
+            console.log('oohnee, error retrieving case status values..');
+            console.log(error);
+            this.error = error;
+            this.caseStatusValues = undefined;
+
+        }
+    }
 
     @wire(getUserFullname, {userId:'$userId'})
     handleGetFullname({error, data}){
@@ -27,4 +50,6 @@ export default class CardForModal extends LightningElement {
         this.showModal = !this.showModal; 
         console.log(`value of showModal after onclick: ${this.showModal}`);
     }
+
+    
 }
